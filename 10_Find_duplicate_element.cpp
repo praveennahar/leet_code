2b8444f174f896de301cb16802899ce0a1efdf89
Praveen nahar <praveennahar9131@gmail.com>
2022-12-30 22:26:56 +0530

    Update 10_Find_duplicate_element.cpp

--- 10_Find_duplicate_element.cpp
+++ 10_Find_duplicate_element.cpp
@@ -24,13 +24,17 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         
-        for(int i=0;i<nums.size();i++){
-            
-            if(nums[abs(nums[i])] < 0)
-                return abs(nums[i]);
-            
-                nums[abs(nums[i])]*= -1;
+        int slow = 0,fast=0;
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+
+        slow=0;
+        while(slow !=fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return 0;
+        return slow;
     } 
 };
