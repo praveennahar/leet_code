d4f4924b93265ac50a98fd0022e49489ee0c3cf6
Praveen nahar <praveennahar9131@gmail.com>
2023-02-05 16:40:30 +0530

    Update 49_SpiralPrint.py

--- 49_SpiralPrint.py
+++ 49_SpiralPrint.py
@@ -26,6 +26,9 @@
 # 1 <= m, n <= 10
 # -100 <= matrix[i][j] <= 100
 
+
+##first approach
+
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         result = []
@@ -43,3 +46,32 @@ class Solution:
                 for line in matrix[::-1]:
                     result.append(line.pop(0))
         return result
+
+# 2nd approach
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        res = []
+        left,right = 0, len(matrix[0])
+        top, bottom = 0, len(matrix)
+
+        while(left<right and top<bottom):
+            for i in range(left,right):
+                res.append(matrix[top][i])
+            top+=1
+
+            for i in range(top,bottom):
+                res.append(matrix[i][right-1])
+            right-=1
+
+            if not(left<right and top<bottom):
+                break
+            
+            for i in range(right-1,left-1,-1):
+                res.append(matrix[bottom-1][i])
+            bottom-=1
+
+            for i in range(bottom-1,top-1,-1):
+                res.append(matrix[i][left])
+            left+=1
+
+        return res
