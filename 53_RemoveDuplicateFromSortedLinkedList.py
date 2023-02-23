cf74c12ac617c6a38874ac268005a6cd32d45212
Praveen nahar <praveennahar9131@gmail.com>
2023-02-23 23:17:53 +0530

    Create 53_RemoveDuplicateFromSortedLinkedList.py

---
+++ 53_RemoveDuplicateFromSortedLinkedList.py
@@ -0,0 +1,70 @@
+# 83. Remove Duplicates from Sorted List
+# Easy
+# 6.8K
+# 240
+# Companies
+# Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+ 
+
+# Example 1:
+
+
+# Input: head = [1,1,2]
+# Output: [1,2]
+# Example 2:
+
+
+# Input: head = [1,1,2,3,3]
+# Output: [1,2,3]
+ 
+
+# Constraints:
+
+# The number of nodes in the list is in the range [0, 300].
+# -100 <= Node.val <= 100
+# The list is guaranteed to be sorted in ascending order.
+
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # if head is None : return head
+
+        curr = head
+        while curr:
+            while curr.next and curr.val == curr.next.val:
+                curr.next = curr.next.next
+            curr = curr.next    
+        return head
+
+
+
+
+
+# Second Approach
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head : return None
+
+        temp = head
+        while temp.next:
+            
+            if temp.val == temp.next.val:
+                temp.next = temp.next.next
+                
+            else:
+                temp = temp.next
+            # head = temp        
+        return head
