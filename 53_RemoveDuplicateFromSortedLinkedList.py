8c5f8030d178b0fea6d547445e2fe40a18ed5120
Praveen nahar <praveennahar9131@gmail.com>
2023-03-10 16:45:30 +0530

    Update 53_RemoveDuplicateFromSortedLinkedList.py

--- 53_RemoveDuplicateFromSortedLinkedList.py
+++ 53_RemoveDuplicateFromSortedLinkedList.py
@@ -65,3 +65,26 @@ class Solution:
                 temp = temp.next
             # head = temp        
         return head
+    
+
+
+    # c++ Solution
+
+# class Solution {
+# public:
+#     ListNode* deleteDuplicates(ListNode* head) {
+#         if (!head) return NULL;
+
+#         ListNode* curr = head;
+
+#         while(curr->next){
+#             if(curr->val == curr->next->val){
+#                 curr->next = curr->next->next;
+#             }
+#             else {
+#             curr = curr->next;
+#             }
+#         }
+#         return head;
+#     }
+# };
