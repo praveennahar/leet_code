6e747f97811e8d7a0a39f493cf33021991986b30
Praveen nahar <praveennahar9131@gmail.com>
2023-03-29 00:31:29 +0530

    Create 85_DeleteMiddleNodeOfLL.cpp

---
+++ 85_DeleteMiddleNodeOfLL.cpp
@@ -0,0 +1,32 @@
+// 2095
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        // if(!)
+        
+        ListNode* dummy = new ListNode(-1);
+        ListNode* slow = dummy;
+        ListNode* fast = head;
+        dummy->next = head;
+        
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        slow->next = slow->next->next;
+        
+        return dummy->next;
+        
+    }
+};
