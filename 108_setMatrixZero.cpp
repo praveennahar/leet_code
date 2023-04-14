e0d740e1f1050ce02e8bd81a56b474fc3f993f86
Praveen nahar <praveennahar9131@gmail.com>
2023-04-14 22:57:05 +0530

    Create 108_setMatrixZero.cpp

---
+++ 108_setMatrixZero.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+         int row[n];// row array
+         int col[m];// col array
+        
+        for(int i=0; i<n; i++){
+            row[i] = 0;
+        }
+        for(int j=0; j<m; j++){
+            col[j] = 0;
+        }
+
+    // Traverse the matrix:
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    // mark ith index of row wih 1:
+                    row[i] = true;
+
+                    // mark jth index of col wih 1:
+                    col[j] = true;
+                }
+            }
+        }
+
+        // Finally, mark all (i, j) as 0
+        // if row[i] or col[j] is marked with 1.
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
